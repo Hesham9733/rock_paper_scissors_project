a1fe9f2e2a7f5feaf531ad4212e86a70ca7dbd40
hesham <heshammahmoud9733@gmail.com>
2022-01-03 19:04:34 +0200

    editing_the_game

--- script.js
+++ script.js
@@ -10,75 +10,83 @@ const aiScore = document.querySelector('.aiScore');
 aiCount.textContent = `The AI Score : `;
 const finalResult = document.querySelector('.resultGame');
 const choices = ['rock', 'paper', 'scissors'];
-function playerChoice(array, index){
+function playerChoice(array, index) {
     const pchoice = array[index];
     return pchoice;
 }
-function aiChoice(array){
+function aiChoice(array) {
     const randomNum = Math.floor(Math.random() * array.length);
-    const achoice = array[randomNum]; 
-    return achoice;
+    const aihoice = array[randomNum];
+    return aihoice;
 }
-function playRound(player, ai){
-    if(player === ai){
-        gameGround.textContent = `It's a DRAW ! you both choosed ${player}`;
-    } else if(player === 'rock' && ai === 'paper'){
-        gameGround.textContent = 'AI Wins ! Paper beats Rock';
-        aiScore.textContent += 'x';
-    } else if(player === 'rock' && ai === 'scissors'){
-        gameGround.textContent = 'You Wins ! Rock beats Scissors';
-        playerScore.textContent += 'x';
-    } else if(player === 'paper' && ai === 'rock'){
-        gameGround.textContent = 'You Wins ! Paper beats Rock';
-        playerScore.textContent += 'x';
-    } else if(player === 'paper' && ai === 'scissors'){
-        gameGround.textContent = 'AI Wins ! Scissors beats Paper';
-        aiScore.textContent += 'x';
-    } else if(player === 'scissors' && ai === 'rock'){
-        gameGround.textContent = 'AI Wins ! Rock beats Scissors';
-        aiScore.textContent += 'x';
-    } else if(player === 'scissors' && ai === 'paper'){
-        gameGround.textContent = 'You Wins ! Scissors beats Paper';
-        playerScore.textContent += 'x';
+function playRound(player, ai) {
+    if (aiScore.textContent.length > 4 || playerScore.textContent.length > 4) {
+        alert(finalResult.textContent);
+    } else {
+        if (player === ai) {
+            gameGround.textContent = `It's a DRAW ! you both choosed ${player}`;
+        } else if (player === 'rock' && ai === 'paper') {
+            gameGround.textContent = 'AI Wins ! Paper beats Rock';
+            aiScore.textContent += 'x';
+        } else if (player === 'rock' && ai === 'scissors') {
+            gameGround.textContent = 'You Wins ! Rock beats Scissors';
+            playerScore.textContent += 'x';
+        } else if (player === 'paper' && ai === 'rock') {
+            gameGround.textContent = 'You Wins ! Paper beats Rock';
+            playerScore.textContent += 'x';
+        } else if (player === 'paper' && ai === 'scissors') {
+            gameGround.textContent = 'AI Wins ! Scissors beats Paper';
+            aiScore.textContent += 'x';
+        } else if (player === 'scissors' && ai === 'rock') {
+            gameGround.textContent = 'AI Wins ! Rock beats Scissors';
+            aiScore.textContent += 'x';
+        } else if (player === 'scissors' && ai === 'paper') {
+            gameGround.textContent = 'You Wins ! Scissors beats Paper';
+            playerScore.textContent += 'x';
+        }
     }
 }
 
 rockBtn.addEventListener('click', () => {
-    playRound(playerChoice(choices,0), aiChoice(choices));
-    if(aiScore.textContent === 'xxxxx'){
+    playRound(playerChoice(choices, 0), aiChoice(choices));
+    if (aiScore.textContent.length == 5) {
         gameGround.textContent = '';
-        finalResult.textContent = 'You Lost ! The AI Win';
-    } else if(playerScore.textContent === 'xxxxx'){
+        playerCount.textContent = '';
+        aiCount.textContent = '';
+        finalResult.textContent = 'You Lost ! The AI Win .';
+    } else if (playerScore.textContent.length == 5) {
         gameGround.textContent = '';
-        finalResult.textContent = 'Congratulations ! You Win';
+        playerCount.textContent = '';
+        aiCount.textContent = '';
+        finalResult.textContent = 'Congratulations ! You Win .';
     }
-
 });
 paperBtn.addEventListener('click', () => {
-    playRound(playerChoice(choices,1), aiChoice(choices));
-    if(aiScore.textContent == 'xxxxx'){
+    playRound(playerChoice(choices, 1), aiChoice(choices));
+    if (aiScore.textContent.length == 5) {
         gameGround.textContent = '';
-        finalResult.textContent = 'You Lost ! The AI Win';
-    } else if(playerScore.textContent == 'xxxxx'){
+        playerCount.textContent = '';
+        aiCount.textContent = '';
+        finalResult.textContent = 'You Lost ! The AI Win .';
+
+    } else if (playerScore.textContent.length == 5) {
         gameGround.textContent = '';
-        finalResult.textContent = 'Congratulations ! You Win';
+        playerCount.textContent = '';
+        aiCount.textContent = '';
+        finalResult.textContent = 'Congratulations ! You Win .';
     }
 });
 scissorsBtn.addEventListener('click', () => {
-    playRound(playerChoice(choices,2), aiChoice(choices));
-    if(aiScore.textContent === 'xxxxx'){
+    playRound(playerChoice(choices, 2), aiChoice(choices));
+    if (aiScore.textContent.length == 5) {
         gameGround.textContent = '';
         playerCount.textContent = '';
         aiCount.textContent = '';
-        playerScore.textContent = '';
-        aiScore.textContent = '';
         finalResult.textContent = 'You Lost ! The AI Win .';
-    } else if(playerScore.textContent === 'xxxxx'){
+    } else if (playerScore.textContent.length == 5) {
         gameGround.textContent = '';
         playerCount.textContent = '';
         aiCount.textContent = '';
-        playerScore.textContent = '';
-        aiScore.textContent = '';
         finalResult.textContent = 'Congratulations ! You Win .';
     }
 });
