8c040276ff5d50c554716179152dc16e5fb04b2f
hesham <heshammahmoud9733@gmail.com>
2022-01-03 17:51:37 +0200

    finish prototype one

--- script.js
+++ script.js
@@ -1,51 +1,78 @@
-function computerPlay() {
-    const playList = ['Rock', 'Paper', 'Scissors'];
-    const computerSelection = playList[Math.floor(Math.random() * (playList.length))];
-    console.log(computerSelection);
-    return computerSelection;
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+const gameGround = document.querySelector('.gameContent');
+const playerCount = document.querySelector('.playerCount');
+const playerScore = document.querySelector('.playerScore');
+playerCount.textContent = `The Player Score : `;
+const aiCount = document.querySelector('.aiCount');
+const aiScore = document.querySelector('.aiScore');
+aiCount.textContent = `The AI Score : `;
+const finalResult = document.querySelector('.resultGame');
+const choices = ['rock', 'paper', 'scissors'];
+function playerChoice(array, index){
+    const pchoice = array[index];
+    return pchoice;
 }
-function playerPlay() {
-    const playerSelection = prompt('Choose between ( Rock - Paper - Scissors ) ?');
-    console.log(playerSelection);
-    return playerSelection;
-    
+function aiChoice(array){
+    const randomNum = Math.floor(Math.random() * array.length);
+    const achoice = array[randomNum]; 
+    return achoice;
 }
-let playerScore = 0;
-let computerScore = 0;
-function playRound(computer,player){
-    if(computer.toLowerCase() === player.toLowerCase()){
-        alert(`It's Draw ! you both choosed ${computer}`);
-    } else if(computer.toLowerCase() == 'rock' && player.toLowerCase() == 'paper'){
-        playerScore += 1;
-        alert('The Player Wins ! Paper beats Rock');
-    } else if(computer.toLowerCase() == 'rock' && player.toLowerCase() == 'scissors'){
-        computerScore += 1;
-        alert('The Computer Wins ! Rock beats Scissors');
-    } else if(player.toLowerCase() == 'rock' && computer.toLowerCase() == 'paper'){
-        computerScore += 1;
-        alert('The Computer Wins ! Paper beats Rock');
-    } else if(player.toLowerCase() == 'rock' && computer.toLowerCase() == 'scissors'){
-        playerScore += 1;
-        alert('The Player Wins ! Rock beats Scissors');
-    } else if(player.toLowerCase() == 'scissors' && computer.toLowerCase() == 'paper'){
-        alert('The Player Wins ! Scissors beats Paper');
-    } else if(computer.toLowerCase() == 'scissors' && player.toLowerCase() == 'paper'){
-        alert('The Computer Wins ! Scissors beats Paper');
-    } else {
-        alert('You entered a wrond choice !');
+function playRound(player, ai){
+    if(player === ai){
+        gameGround.textContent = `It's a DRAW ! you both choosed ${player}`;
+    } else if(player === 'rock' && ai === 'paper'){
+        gameGround.textContent = 'AI Wins ! Paper beats Rock';
+        aiScore.textContent += 'x';
+    } else if(player === 'rock' && ai === 'scissors'){
+        gameGround.textContent = 'You Wins ! Rock beats Scissors';
+        playerScore.textContent += 'x';
+    } else if(player === 'paper' && ai === 'rock'){
+        gameGround.textContent = 'You Wins ! Paper beats Rock';
+        playerScore.textContent += 'x';
+    } else if(player === 'paper' && ai === 'scissors'){
+        gameGround.textContent = 'AI Wins ! Scissors beats Paper';
+        aiScore.textContent += 'x';
+    } else if(player === 'scissors' && ai === 'rock'){
+        gameGround.textContent = 'AI Wins ! Rock beats Scissors';
+        aiScore.textContent += 'x';
+    } else if(player === 'scissors' && ai === 'paper'){
+        gameGround.textContent = 'You Wins ! Scissors beats Paper';
+        playerScore.textContent += 'x';
     }
-    return playerScore,computerScore;
-}
-for(i=0; i < 5; i++){
-    playRound(computerPlay(),playerPlay());
-}
-if(playerScore > computerScore){
-    alert('The Player is The Winner !!!!!!!!! ');
-} else if(computerScore > playerScore){
-    alert('The Computer is The Winner !!!!!!!!!');
-} else if(playerScore === computerScore){
-    alert("It's Draw ! Play again !");
 }
 
+rockBtn.addEventListener('click', () => {
+    playRound(playerChoice(choices,0), aiChoice(choices));
+    if(aiScore.textContent === 'xxxxx'){
+        gameGround.textContent = '';
+        finalResult.textContent = 'You Lost ! The AI Win';
+    } else if(playerScore.textContent === 'xxxxx'){
+        gameGround.textContent = '';
+        finalResult.textContent = 'Congratulations ! You Win';
+    }
+
+});
+paperBtn.addEventListener('click', () => {
+    playRound(playerChoice(choices,1), aiChoice(choices));
+    if(aiScore.textContent == 'xxxxx'){
+        gameGround.textContent = '';
+        finalResult.textContent = 'You Lost ! The AI Win';
+    } else if(playerScore.textContent == 'xxxxx'){
+        gameGround.textContent = '';
+        finalResult.textContent = 'Congratulations ! You Win';
+    }
+});
+scissorsBtn.addEventListener('click', () => {
+    playRound(playerChoice(choices,2), aiChoice(choices));
+    if(aiScore.textContent === 'xxxxx'){
+        gameGround.textContent = '';
+        finalResult.textContent = 'You Lost ! The AI Win';
+    } else if(playerScore.textContent === 'xxxxx'){
+        gameGround.textContent = '';
+        finalResult.textContent = 'Congratulations ! You Win';
+    }
+});
 
 
